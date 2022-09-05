62d9890a0f89ce745a71773715f8ce69f91047f9
henriquemattia <mattia.henrique@gmail.com>
2022-09-05 18:30:08 -0300

    Adiçao de página dinamica

--- src/pages/BuyProduct/BuyProduct.jsx
+++ src/pages/BuyProduct/BuyProduct.jsx
@@ -14,51 +14,65 @@ import { useParams } from 'react-router-dom';
 
 function Product() {
 
-  const [ produtos, setProdutos ] = useState([])
+  const [produtos, setProdutos] = useState([])
+  const [prodMascu, setProdMascu] = useState([])
+  const [prodfemi, setProdfemi] = useState([])
 
-  useEffect(()=>{
+  useEffect(() => {
     fetch('http://127.0.0.1:5000/produtos')
       .then(res => res.json())
       .then(data => setProdutos(data))
-      }, [])
+  }, [])
 
   const parms = useParams()
-      console.log(parms.id);
 
-  const producttt = produtos.find((p)=> p.rota === parms.id)
-  console.log(producttt);
+  produtos.find((p) => p.rota === parms.id)
+  useEffect(() => {
+    produtos.map((nam) => {
+      if (nam.masculino.rota == parms.id) {
+        setProdMascu(nam.masculino)
+      }
+      else if (nam.feminino.rota == parms.id) {
+        setProdfemi(nam.feminino)
+      }
+      else return console.log('erro aqui')
+    }
+    )
+  }, [produtos])
+  // console.log(prodMascu, "masculino");
+  // console.log(prodfemi, "feminino");
 
 
   return (
     <>
-    <Topbar />
-    <Container>
-    <Image src={camiseta} alt="homerm" className='imagem1'/>
-    <Row>
-      <Col><Image src={camiseta} alt="homerm" className='abaixo'/></Col>
-      <Col><Image src={camiseta} alt="homerm" className='abaixo'/></Col>
-      <Col><Image src={camiseta} alt="homerm" className='abaixo'/></Col>
-      <Col><Image src={camiseta} alt="homerm" className='abaixo'/></Col>
-    </Row>
-    </Container>
+      <Topbar />
+      <Container>
+        <Image src={camiseta} alt="homerm" className='imagem1' />
+        <Row>
+          <Col><Image src={camiseta} alt="homerm" className='abaixo' /></Col>
+          <Col><Image src={camiseta} alt="homerm" className='abaixo' /></Col>
+          <Col><Image src={camiseta} alt="homerm" className='abaixo' /></Col>
+          <Col><Image src={camiseta} alt="homerm" className='abaixo' /></Col>
+        </Row>
+      </Container>
 
-    <Container>
-      <h2 className='name'>{producttt?.name}</h2>
-      <span className='tamanho desc_price'>R$ 369</span> 
-      <span className='tamanho price ' > R$ 184,50</span>
-      <p>Ultimas unidades desse modelo aproveite</p>
-    </Container>
-    <Container>
-      <span><button> - 1 + </button> <button>COMPRAR</button></span>
-    </Container>
-    <br />
-    <Container>
-      <p>CALCULE O VALOR DO FRETE</p>
-      <span><button>00000-000</button> <button>CALCULAR</button></span>
-    </Container>
-    <br />
-    <Footer />
-    
+      <Container>
+        <h2 className='name'>{prodMascu.name || prodfemi.name}</h2>
+        <span className='tamanho desc_price'>{prodMascu.valorDesc || prodfemi.valorDesc}</span>
+        <span className='tamanho price ' >{prodMascu.valor || prodfemi.valor}</span>
+        <p>Ultimas unidades desse modelo aproveite</p>
+      </Container>
+      <Container>
+        <span><button> - 1 + </button> <button>COMPRAR</button></span>
+      </Container>
+      <br />
+      <Container>
+        <p>CALCULE O VALOR DO FRETE</p>
+        <span><button>00000-000</button> <button>CALCULAR</button></span>
+      </Container>
+      <br />
+      <Footer />
+
     </>
   )
 }
