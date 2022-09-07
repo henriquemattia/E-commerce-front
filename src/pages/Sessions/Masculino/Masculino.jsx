17c88f3b4e39f13701427b07f873d222273f2e65
henriquemattia <mattia.henrique@gmail.com>
2022-09-07 19:55:16 -0300

    Adicionando conteudo a pagina de Masculinos

--- src/pages/Sessions/Masculino/Masculino.jsx
+++ src/pages/Sessions/Masculino/Masculino.jsx
@@ -1,12 +1,64 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Row from 'react-bootstrap/esm/Row';
+import Container from 'react-bootstrap/esm/Container';
+
+import camisa from '../../../images/products/camisetao-laranja.webp'
+import Molde from '../../AllProducts/Molde/Molde'
+
 import Topbar from '../../../components/NavBar/NavBar';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+// import './styles.css'
 
-function Masculino() {
-    return (
-        <Topbar />
-      )
+
+//colcoar tudo dentro de uma vide para tentar alinhar tudo confome vai almentando a tela
+//ver soibre RATIO opara as imagens respoeotarem um tanmanho
+
+
+ function Masculino() {
+
+  const [ masculino, setMasculino ] = useState([])
+  // const [ prodM, setProdM] = useState([])
+  // const [ prodF, setProdF] = useState([])
+
+  
+
+
+
+   useEffect (()=>{
+    async function fetchData(){
+    await fetch('http://127.0.0.1:5000/masculino')
+      .then(res => res.json())
+      .then(data => setMasculino(data))}
+      fetchData()
+      }, [])//deixando vazio o componente será atualizado somente uma vez "quando a pagina for carregada", entao nao importa qunatas vezs nossa variavel for alterada ele nao vai usar o useEffect! 
+
+  
+    return(
+    <>
+      <Topbar />
+      <br />
+      <br />
+      <br />
+        <Container>
+        <Row>
+        {masculino.map((prod, index) => {
+            return (
+              <Molde 
+              key={index}
+              image={camisa}
+              name={prod.name}
+              valor={prod.valor}
+              route={<Link className='tag-a'to={`/produtos/${prod.rota}`}>COMPRAR</Link>}
+              />
+            )  
+          })}
+          </Row>
+      </Container>
+
+    </>
+  )
 }
+
 
 export default Masculino;
