d58fe891795ae94145bfda07fca3eedb483bd90c
henriquemattia <mattia.henrique@gmail.com>
2022-10-12 17:05:47 -0300

    Refatoranto pt-2

--- src/components/Carrossel/Carrossel.jsx
+++ src/components/Carrossel/Carrossel.jsx
@@ -1,58 +1,59 @@
+// Styles
 import Carousel from 'react-bootstrap/Carousel';
+import './styles.css'
 
+
+//Imagens do carrossel
 import Masculino from '../../images/carrossel/masculino-carrossel.jpg'
 import Feminino from '../../images/carrossel/feminino-carrossel.jpg'
 import Acessorios from '../../images/carrossel/acessorios-carrossel.jpg'
 
+function Carrossel() {
+  return (
+    <Carousel className='margin-top'>
+      <Carousel.Item className='gg'>
+        <img
+          className="d-block w-100"
+          src={Feminino}
+          alt="Mulher com um vestido preto seguranco uma bolsa preta, andando pelas ruas"
+        />
 
-import './styles.css'
-
-
-function Carrossel () {
-    return (
-        <Carousel className='mt'>
-
-          <Carousel.Item className='gg'>
-            <img
-              className="d-block w-100"
-              src={Feminino}
-              alt="First slide"
-            />
-            <Carousel.Caption>
-              <h5 className='titulo-1'>MODA FEMININA</h5>
-              <p>Novidades na moda Inverno/Verão</p>
-            </Carousel.Caption>
-          </Carousel.Item>
+        <Carousel.Caption>
+          <h5>MODA FEMININA</h5>
+          <p>Novidades na moda Inverno/Verão</p>
+        </Carousel.Caption>
+      </Carousel.Item>
 
 
-          <Carousel.Item className='gg'>
-            <img
-              className="d-block w-100"
-                src={Masculino}
-              alt="Second slide"
-            />
-            <Carousel.Caption>
-              <h5>MODA MASCULINA</h5>
-              <p>Estilo e classe, você só encontra aqui!</p>
-            </Carousel.Caption>
-          </Carousel.Item>
+      <Carousel.Item className='gg'>
+        <img
+          className="d-block w-100"
+          src={Masculino}
+          alt="Homem com barba comprida e camiseta preta, tomando coca-cola em um bar"
+        />
+
+        <Carousel.Caption>
+          <h5>MODA MASCULINA</h5>
+          <p>Estilo e classe, você só encontra aqui!</p>
+        </Carousel.Caption>
+      </Carousel.Item>
 
 
-          <Carousel.Item className='gg'>
-            <img
-              className="d-block w-100"
-              src={Acessorios}
-              alt="Third slide"
-            />
-    
+      <Carousel.Item className='gg'>
+        <img
+          className="d-block w-100"
+          src={Acessorios}
+          alt="Sapato marrom, relogio redondo, e escovas de canelo sobre uma mesa de madeira"
+        />
 
-            <Carousel.Caption>
-              <h5>ACESSÓRIOS E CALÇADOS</h5>
-              <p>Tecnoligia e conforto</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-        </Carousel>
-      );
-    }
+        <Carousel.Caption>
+          <h5>ACESSÓRIOS E CALÇADOS</h5>
+          <p>Tecnoligia e conforto</p>
+        </Carousel.Caption>
+        
+      </Carousel.Item>
+    </Carousel>
+  );
+}
 
 export default Carrossel;
